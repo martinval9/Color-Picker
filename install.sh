83250e6ff5e7a6ff6cb14ba44c39d3b515a6e8b6
martinval9 <68918892+martinval9@users.noreply.github.com>
2021-07-18 12:00:40 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo '▒█▀▀█ █▀▀█ █░░ █▀▀█ █▀▀█ 　 ▒█▀▀█ ░▀░ █▀▀ █░█ █▀▀ █▀▀█' 
+echo '▒█░░░ █░░█ █░░ █░░█ █▄▄▀ 　 ▒█▄▄█ ▀█▀ █░░ █▀▄ █▀▀ █▄▄▀' 
+echo '▒█▄▄█ ▀▀▀▀ ▀▀▀ ▀▀▀▀ ▀░▀▀ 　 ▒█░░░ ▀▀▀ ▀▀▀ ▀░▀ ▀▀▀ ▀░▀▀'
+
+sudo cp colorpicker.desktop /usr/share/applications
+
+echo 'Color Picker Instalado :D'
